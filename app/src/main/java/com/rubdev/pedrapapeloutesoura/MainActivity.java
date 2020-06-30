9b573d7e6e99450b797544266ac8e5e20420d546
Rubens MA <rubao.ma@gmail.com>
2020-06-30 09:14:05 -0300

    Create interface app

---
+++ app/src/main/java/com/rubdev/pedrapapeloutesoura/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rubdev.pedrapapeloutesoura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
