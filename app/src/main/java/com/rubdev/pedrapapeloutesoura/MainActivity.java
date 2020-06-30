7fcb18aea287ca2cd8719654435fd3b640b69f2d
Rubens MA <rubao.ma@gmail.com>
2020-06-30 09:43:45 -0300

    Insert onclick method in the selected images

--- app/src/main/java/com/rubdev/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/rubdev/pedrapapeloutesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.rubdev.pedrapapeloutesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selecionadoPedra(View view){
+        System.out.println("Selecionado pedra");
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoPapel(View view){
+        System.out.println("Selecionado papel");
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionadoTesoura(View view){
+        System.out.println("Selecionado tesoura");
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada( String opcaoSelecionada){
+        System.out.println("item selecionado: " + opcaoSelecionada);
+    }
+
+
 }
